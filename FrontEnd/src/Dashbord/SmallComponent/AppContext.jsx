ad4c4b2b75dd2b944b19bf0c0592d0db8bc711f5
Momen <touristmomen@gmail.com>
2025-05-23 20:24:40 +0600

    eslint, proptypes updated

--- FrontEnd/src/Dashbord/SmallComponent/AppContext.jsx
+++ FrontEnd/src/Dashbord/SmallComponent/AppContext.jsx
@@ -1,22 +1,14 @@
-import React, { createContext, useState } from "react";
-import PropTypes from "prop-types";
-
-// Create the context
+import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 // Create a provider component
 export const AppProvider = ({ children }) => {
-  // const [state, setState] = useState({ port: "http://localhost:5001" });
-  const [state, setState] = useState({ port: "https://api.tojonews.com" });
+  const [state, setState] = useState({ port: "http://localhost:5002" });
+  // const [state, setState] = useState({ port: "https://api.tojonews.com" });
 
   return (
     <AppContext.Provider value={{ state, setState }}>
       {children}
     </AppContext.Provider>
   );
-};
-
-// Define prop types for AppProvider
-AppProvider.propTypes = {
-  children: PropTypes.node.isRequired,
 };
